68342e2e1b8a800cf3bc74d4264a89277bc3a166
randomcoder67 <minecraft1111311@gmail.com>
2023-06-01 15:15:09 +0100

    fixed panel stream check programs

--- system/panel/panelChudLogic.sh
+++ system/panel/panelChudLogic.sh
@@ -4,22 +4,19 @@
 
 curl https://www.youtube.com/@ChudLogic/live > ~/Programs/output/.streams/panel/chudyoutube.html
 curl https://www.twitch.tv/chudlogic > ~/Programs/output/.streams/panel/chudtwitch.html
-if grep -q "Pop-out chat" ~/Programs/output/.streams/panel/chudyoutube.html
+
+
+if grep -q "isLiveBroadcast" ~/Programs/output/.streams/panel/chudtwitch.html
+then
+	echo "<txt><span foreground='#da4939'>  </span></txt><txtclick>mpv --title='Chud Logic' https://www.twitch.tv/chudlogic</txtclick><tool>"
+elif grep -q "Pop-out chat" ~/Programs/output/.streams/panel/chudyoutube.html
 then
 	if grep -q "Live in" ~/Programs/output/.streams/panel/chudyoutube.html
 	then
-		if grep -q "isLiveBroadcast" ~/Programs/output/.streams/panel/chudtwitch.html
-		then
-			echo echo "<txt><span foreground='#da4939'>  </span></txt><txtclick>$HOME/Programs/terminal/alias/chud.sh</txtclick><tool>"
-		else
-			echo "  "
-		fi
+		echo "  "
 	else
-		echo echo "<txt><span foreground='#da4939'>  </span></txt><txtclick>mpv https://www.youtube.com/@ChudLogic/live</txtclick><tool>"
+		echo "<txt><span foreground='#da4939'>  </span></txt><txtclick>mpv https://www.youtube.com/@ChudLogic/live</txtclick><tool>"
 	fi
-elif grep -q "isLiveBroadcast" ~/Programs/output/.streams/panel/chudtwitch.html
-then
-	echo echo "<txt><span foreground='#da4939'>  </span></txt><txtclick>$HOME/Programs/terminal/alias/chud.sh</txtclick><tool>"
 else
 	echo "  "
 fi
