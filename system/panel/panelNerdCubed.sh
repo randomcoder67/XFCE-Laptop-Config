6f66d48320bb85c123470eadf5cc0650dfa2876d
randomcoder67 <minecraft1111311@gmail.com>
2023-06-01 15:15:09 +0100

    fixed panel stream check programs

--- system/panel/panelNerdCubed.sh
+++ system/panel/panelNerdCubed.sh
@@ -5,7 +5,7 @@
 curl https://www.twitch.tv/nerdcubed > ~/Programs/output/.streams/panel/nerdcubed.html
 if grep -q isLiveBroadcast ~/Programs/output/.streams/panel/nerdcubed.html
 then
-	echo "<txt><span foreground='#da4939'>  </span></txt><txtclick>$HOME/Programs/terminal/alias/nerdcubed.sh</txtclick><tool>"
+	echo "<txt><span foreground='#da4939'>  </span></txt><txtclick>mpv --title=NerdCubed https://www.twitch.tv/nerdcubed</txtclick><tool>"
 else
 	echo "  "
 fi
