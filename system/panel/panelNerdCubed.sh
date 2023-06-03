0765e0eec1528b4955eebd3447797db53cefe03b
randomcoder67 <randomcoder67@gmail.com>
2023-06-03 18:01:44 +0100

    Added tooltips to panel stream monitors

--- system/panel/panelNerdCubed.sh
+++ system/panel/panelNerdCubed.sh
@@ -6,6 +6,8 @@ curl https://www.twitch.tv/nerdcubed > ~/Programs/output/.streams/panel/nerdcube
 if grep -q isLiveBroadcast ~/Programs/output/.streams/panel/nerdcubed.html
 then
 	echo "<txt><span foreground='#da4939'>  </span></txt><txtclick>mpv --title=NerdCubed https://www.twitch.tv/nerdcubed</txtclick><tool>"
+	echo "<tool>Twitch</tool>"
 else
-	echo "  "
+	echo "<txt>  </txt>"
+	echo "<tool>Not Live</tool>"
 fi
