a89b2073783d68610e81898ae47a37a18c9685ab
randomcoder67 <esgascoigne@gmail.com>
2023-06-18 11:53:29 +0100

    minor changes

--- system/rofi/rofiBookmarks.sh
+++ system/rofi/rofiBookmarks.sh
@@ -1,21 +1,28 @@
 #!/usr/bin/env bash
 
-item=$(cat ~/Programs/output/updated/bookmarks.txt | rofi -kb-custom-1 "Ctrl+a" -kb-custom-2 "Ctrl+w" -kb-custom-3 "Shift+Return" -dmenu -i -p "Bookmarks")
+item=$(awk -F 'DELIM' '{print $1}' ~/Programs/output/updated/bookmarks.txt | rofi -kb-custom-1 "Ctrl+a" -kb-custom-2 "Ctrl+w" -kb-custom-3 "Shift+Return" -dmenu -i -p "Bookmarks")
 status=$?
 if [ $status -eq 10 ]; then
 	itemToAdd=$(rofi -dmenu -p "Enter New Bookmark")
-	if ! [[ $itemToAdd == "" ]]; then
-		echo $itemToAdd >> ~/Programs/output/updated/bookmarks.txt
+	[[ "$itemToAdd" == "" ]] && exit
+	itemAlias=$(rofi -dmenu -p "Enter Alias (Leave blank for no alias)")
+	if [[ "$itemAlias" == "" ]]; then
+		echo "$itemToAdd""DELIM""$itemToAdd" >> ~/Programs/output/updated/bookmarks.txt
+	else
+		echo "$itemAlias""DELIM""$itemToAdd" >> ~/Programs/output/updated/bookmarks.txt
 	fi
 elif [ $status -eq 11 ]; then
 	itemA=${item//"|"/"[|]"}
 	itemA=${item//"\\"/"[\\]"}
-	echo a
 	echo $itemA
 	sed -i "\|^$itemA$|d" ~/Programs/output/updated/bookmarks.txt
 elif [ $status -eq 12 ]; then
 	#sleep 1
-	xdotool type "$item"
+	xdotool keyup Shift
+	itemA=$(echo "$item" | sed 's/\[/\\[/g' | sed 's/\]/\\]/g')
+	toType=$(grep "$itemA" ~/Programs/output/updated/bookmarks.txt | awk -F 'DELIM' '{print $2}' | tr -d '\n')
+	xdotool type "$toType"
 else
-	echo $item | xclip -selection c
+	itemA=$(echo "$item" | sed 's/\[/\\[/g' | sed 's/\]/\\]/g')
+	grep "$itemA" ~/Programs/output/updated/bookmarks.txt | awk -F 'DELIM' '{print $2}' | tr -d '\n' | xclip -selection c
 fi
