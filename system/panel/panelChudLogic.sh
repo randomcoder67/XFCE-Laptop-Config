a31b3e7f4708a8ae6f7c3267050d6f8a7e7d4c0d
randomcoder67 <minecraft1111311@gmail.com>
2023-06-30 19:14:41 +0100

    some changes to dotfiles, added -t option to panel stream scrips to make them compatible with tint2, remove a lot of unnecessary code from checkfiles.sh, added lua files to addIcons.sh

--- system/panel/panelChudLogic.sh
+++ system/panel/panelChudLogic.sh
@@ -8,23 +8,48 @@ curl https://www.twitch.tv/chudlogic > ~/Programs/output/.streams/panel/chudtwit
 
 if grep -q "isLiveBroadcast" ~/Programs/output/.streams/panel/chudtwitch.html
 then
-	echo "<txt><span foreground='#da4939'>  </span></txt><txtclick>mpv --title='Chud Logic' https://www.twitch.tv/chudlogic</txtclick>"
-	echo "<tool>Twitch</tool>"
+	if [[ "$1" == "-t" ]]; then
+		echo "<span foreground='#da4939'>  </span>"
+		echo "twitch" > "$XDG_STATE_HOME/streams/chud.txt"
+	else
+		echo "<txt><span foreground='#da4939'>  </span></txt><txtclick>mpv --title='Chud Logic' https://www.twitch.tv/chudlogic</txtclick>"
+		echo "<tool>Twitch</tool>"
+	fi
 elif grep -q "Pop-out chat" ~/Programs/output/.streams/panel/chudyoutube.html
 then
 	if grep -q "Live in" ~/Programs/output/.streams/panel/chudyoutube.html
 	then
-		echo "<txt>  </txt>"
-		echo "<tool>Stream Schedueled</tool>"
+		if [[ "$1" == "-t" ]]; then
+			echo "<span foreground='#e6e1dc'>  </span>"
+			echo "notLive" > "$XDG_STATE_HOME/streams/chud.txt"
+		else
+			echo "<txt>  </txt>"
+			echo "<tool>Stream Schedueled</tool>"
+		fi
 	elif grep -q "Waiting for Chud Logic" ~/Programs/output/.streams/panel/chudyoutube.html
 	then
-		echo "<txt>  </txt>"
-		echo "<tool>Waiting for Chud Logic</tool>" 
+		if [[ "$1" == "-t" ]]; then
+			echo "<span foreground='#e6e1dc'>  </span>"
+			echo "notLive" > "$XDG_STATE_HOME/streams/chud.txt"
+		else
+			echo "<txt>  </txt>"
+			echo "<tool>Not Live</tool>"
+		fi 
 	else
-		echo "<txt><span foreground='#da4939'>  </span></txt><txtclick>mpv https://www.youtube.com/@ChudLogic/live</txtclick>"
-		echo "<tool>YouTube</tool>"
+		if [[ "$1" == "-t" ]]; then
+			echo "<span foreground='#da4939'>  </span>"
+			echo "youtube" > "$XDG_STATE_HOME/streams/chud.txt"
+		else
+			echo "<txt><span foreground='#da4939'>  </span></txt><txtclick>mpv https://www.youtube.com/@ChudLogic/live</txtclick>"
+			echo "<tool>YouTube</tool>"
+		fi
 	fi
 else
-	echo "<txt>  </txt>"
-	echo "<tool>Not Live</tool>"
+	if [[ "$1" == "-t" ]]; then
+		echo "<span foreground='#e6e1dc'>  </span>"
+		echo "notLive" > "$XDG_STATE_HOME/streams/chud.txt"
+	else
+		echo "<txt>  </txt>"
+		echo "<tool>Not Live</tool>"
+	fi
 fi
