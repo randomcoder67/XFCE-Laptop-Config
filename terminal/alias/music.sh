dcc9d8cde844a3fdcd8435f73e06f7a4d4b00bcd
randomcoder67 <minecraft1111311@gmail.com>
2023-07-16 13:02:23 +0100

    minor changes and added arch install guide

--- terminal/alias/music.sh
+++ terminal/alias/music.sh
@@ -3,5 +3,5 @@
 if [[ "$1" == "" ]]; then
 	/usr/bin/mpv --title='${metadata/title}'\ -\ '${metadata/artist}' --shuffle --no-resume-playback --loop-playlist "$HOME/Music/curPlaylist" & disown
 elif [[ "$1" == "-a" ]]; then
-	find "$HOME/Music/" -type f -maxdepth 1 | sort | grep -i "$2" | xargs -d '\n' /usr/bin/mpv --title='${metadata/title}'\ -\ '${metadata/artist}' --shuffle --loop-playlist --no-resume-playback & disown
+	find "$HOME/Music/" -type f -maxdepth 1 | sort | grep -iE "$2" | xargs -d '\n' /usr/bin/mpv --title='${metadata/title}'\ -\ '${metadata/artist}' --shuffle --loop-playlist --no-resume-playback & disown
 fi
