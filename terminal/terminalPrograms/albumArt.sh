cdc6585ed8f69438dbc6b9b95e48d9d98a98944e
randomcoder67 <randomcoder67@gmail.com>
2023-07-16 23:33:37 +0100

    fixed albumArt.sh

--- terminal/terminalPrograms/albumArt.sh
+++ terminal/terminalPrograms/albumArt.sh
@@ -1,33 +1,39 @@
 #!/usr/bin/env bash
 
+tempDirectory="$HOME/Programs/output/.temp/" # Set directory to store the files while you view them
+
 # Makes collage of all album art in directory
 
 IFS=$'\n'
-files=( $(find . -maxdepth 1 | grep -v ".aGay" | grep -vP "^.$") ) # Get files excluding useless stuff
+files=( $(find "$HOME/Music/curPlaylist" -mindepth 1 | grep -vP "^.$") ) # Get files excluding useless stuff
 
-mkdir -p art/art2 # Make directories (after getting files so they aren't in the files array)
+mkdir -p "${tempDirectory}art/art2" # Make directories
 
 for i in ${!files[@]}; do # Extract album art
-	ffmpeg -i "${files[$i]}" -an -c:v copy art/$i.jpg 2>/dev/null >> /dev/null
+	ffmpeg -i "${files[$i]}" -an -c:v copy "${tempDirectory}art/$i.jpg" 2>/dev/null >> /dev/null
 done
 
 if [[ $1 == "-d" ]]; then # Checking for duplicates
 	declare -A filesChecking
 	for i in ${!files[@]}; do
-		hash=$(sha256sum "art/$i.jpg" | cut -d " " -f1)
-		filesChecking["$hash"]="art/$i"
+		hash=$(sha256sum "${tempDirectory}art/$i.jpg" | cut -d " " -f1)
+		filesChecking["$hash"]="${tempDirectory}art/$i"
 	done
 fi
 
 #for x in "${!filesChecking[@]}"; do printf "[%q]=%q\n" "$x" "${filesChecking[$x]}" ; done
 
+# Convert files 
 if [[ $1 == "-d" ]]; then
 	for x in "${!filesChecking[@]}"; do
-		convert "${filesChecking[$x]}".jpg -resize 1400x1400 art/"$(echo ${filesChecking[$x]} | sed -r 's/art/art2/g')".jpg
+		convert "${filesChecking[$x]}".jpg -resize 1400x1400 "${tempDirectory}art/$(echo ${filesChecking[$x]} | sed -r 's/art/art2/g')".jpg
 	done
 else
-	convert art/*.jpg -resize 1400x1400 art/art2/*.jpg
+	convert "${tempDirectory}"art/*.jpg -resize 1400x1400 "${tempDirectory}"art/art2/*.jpg
 fi
-montage -tile 0x0 -geometry +0+0 art/art2/*.jpg art/art2/output.jpg
-ristretto art/art2/output.jpg
-rm -rf ./art
+# Create final images
+montage -tile 0x0 -geometry +0+0 "${tempDirectory}"art/art2/*.jpg "${tempDirectory}"art/art2/output.jpg
+# Display image 
+ristretto "${tempDirectory}"art/art2/output.jpg
+# Once user quits ristretto, delete the temp directories
+rm -rf "$tempDirectory"/art
