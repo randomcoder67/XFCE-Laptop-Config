2300209235fb2a8ea2adf807ce116d4a7f906881
randomcoder67 <minecraft1111311@gmail.com>
2023-07-19 20:38:29 +0100

    made some changes to make setup work with desktop 2

--- system/panel/cputemp.sh
+++ system/panel/cputemp.sh
@@ -4,6 +4,10 @@
 DEBUG=0
 
 CPUTEMP=$(sensors | grep -Eo 'Package id 0:.{0,10}' | grep -Eo '[1-9].{0,1}' | sed 's/$/°C/')
+if [[ "$CPUTEMP" == "" ]]; then
+	CPUTEMP=$(sensors | grep CPUTIN | grep -Eo '[1-9].{3}' | head -1 | sed 's/$/°C/')
+fi
+	
 
 echo "<txt>$CPUTEMP </txt><txtclick>alacritty -e btop</txtclick>"
 echo "<tool>btop</tool>"
