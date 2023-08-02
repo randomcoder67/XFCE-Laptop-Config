83c4b3f45e2be9ad7db5943d05c54db885129494
randomcoder67 <minecraft1111311@gmail.com>
2023-08-02 22:34:31 +0100

    added ability to use sha512 with hashfolder (-l)

--- terminal/alias/hashfolder.sh
+++ terminal/alias/hashfolder.sh
@@ -1,10 +1,19 @@
 #!/usr/bin/env bash
 
-# Script to get sha256 hashes of all files in a directory 
+# Script to get sha256/sha512 hashes of all files in a directory 
 
 IFS=$'\n'
-filesA=( $(find "$1" -type f | sort) )
 
-for fileA in "${filesA[@]}"; do
-	sha256sum "$fileA"
-done
+if [[ "$1" == "-h" ]]; then
+	echo "Usage: hashfolder [-l] folder/ (-l for sha512 instead of sha256)"
+elif [[ "$1" == "-l" ]]; then
+	filesA=( $(find "$2" -type f | sort) )
+	for fileA in "${filesA[@]}"; do
+		sha512sum "$fileA"
+	done
+else
+	filesA=( $(find "$1" -type f | sort) )
+	for fileA in "${filesA[@]}"; do
+		sha256sum "$fileA"
+	done
+fi
