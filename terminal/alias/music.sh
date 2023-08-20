d38aefcb0564ca94ed503b8e4926870e7ede3308
randomcoder67 <randomcoder67@gmail.com>
2023-08-20 16:34:40 +0100

    bashrc made changes to trim_history, it now removes a lot more, see comments for detail

--- terminal/alias/music.sh
+++ terminal/alias/music.sh
@@ -4,8 +4,8 @@
 
 # If no argument given, shuffle music from current playlist 
 if [[ "$1" == "" ]]; then
-	/usr/bin/mpv --title='${metadata/title}'\ -\ '${metadata/artist}' --shuffle --no-resume-playback --loop-playlist "$HOME/Music/curPlaylist" & disown
+	/usr/bin/mpv --really-quiet --title='${metadata/title}'\ -\ '${metadata/artist}' --shuffle --no-resume-playback --loop-playlist "$HOME/Music/curPlaylist" & disown
 # Shuffle music of a particular artist (or artists with | to delimit)
 elif [[ "$1" == "-a" ]]; then
-	find "$HOME/Music/" -type f -maxdepth 1 | sort | grep -iE "$2" | xargs -d '\n' /usr/bin/mpv --title='${metadata/title}'\ -\ '${metadata/artist}' --shuffle --loop-playlist --no-resume-playback & disown
+	find "$HOME/Music/" -maxdepth 1 -type f | sort | grep -iE "$2" | xargs -d '\n' /usr/bin/mpv --really-quiet --title='${metadata/title}'\ -\ '${metadata/artist}' --shuffle --loop-playlist --no-resume-playback & disown
 fi
