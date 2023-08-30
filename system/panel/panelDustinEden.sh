ced585ea5f102b3954cf781d6a3b211c60f03d74
randomcoder67 <randomcoder67@gmail.com>
2023-08-30 17:17:39 +0100

    added -d option to panel scripts, makes them output with dracula colours not railscasts colours

--- system/panel/panelDustinEden.sh
+++ system/panel/panelDustinEden.sh
@@ -2,21 +2,31 @@
 
 # Panel Dustin Eden stream monitor
 
+notLiveColour="e6e1dc"
+liveColour="da4939"
+upcomingColour="a5c261"
+
+if [[ "$1" == "-d" ]]; then
+	notLiveColour="fefef8"
+	liveColour="e64747"
+	upcomingColour="8CFF82"
+fi
+
 #curl "https://www.twitch.tv/dustineden" > ~/Programs/output/.streams/panel/dustinedenTwitch.html
 #if grep -q isLiveBroadcast ~/Programs/output/.streams/panel/dustinedenTwitch.html
 if yt-dlp --write-info-json --skip-download https://www.twitch.tv/dustineden -P ~/Programs/output/.streams/panel -o "dustinedenTwitchMetadata.%(ext)s"
 then
 	if [[ "$1" == "-t" ]]; then
-		echo "<span foreground='#da4939'>  </span>"
+		echo "<span foreground='#$liveColour'>  </span>"
 		echo "twitch" > "$XDG_STATE_HOME/streams/dustineden.txt"
 	else
-		echo "<txt><span foreground='#da4939'>  </span></txt><txtclick>$HOME/Programs/system/panel/streamLauncher.sh dustinedenTwitch</txtclick>"
+		echo "<txt><span foreground='#$liveColour'>  </span></txt><txtclick>$HOME/Programs/system/panel/streamLauncher.sh dustinedenTwitch</txtclick>"
 		streamTitle=$(cat ~/Programs/output/.streams/panel/dustinedenTwitchMetadata.info.json | jq -r .description | sed 's/&/and/g')
 		echo "<tool>Twitch - $streamTitle</tool>"
 	fi
 else
 	if [[ "$1" == "-t" ]]; then
-		echo "<span foreground='#e6e1dc'>  </span>"
+		echo "<span foreground='#$notLiveColour'>  </span>"
 		echo "notLive" > "$XDG_STATE_HOME/streams/dustineden.txt"
 	else
 		echo "<txt>  </txt><txtclick>firefox --new-tab 'https://www.twitch.tv/dustineden/videos?filter=archives&sort=time'</txtclick>"
