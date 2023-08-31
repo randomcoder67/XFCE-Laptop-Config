9ceafea0aa542d75f858f911c0bd27166216079d
randomcoder67 <minecraft1111311@gmail.com>
2023-08-31 10:02:27 +0100

    moved bunch of files

--- archive/publicCodeArchive/readdirC.c
+++ archive/publicCodeArchive/readdirC.c
@@ -4,7 +4,7 @@
 int main() {
 	DIR *d;
 	struct dirent *dir;
-	d = opendir("/home/ethan/Music/curPlaylist/");
+	d = opendir("/home/USER/Music/curPlaylist/");
 	if (d) {
 		while ((dir = readdir(d)) != NULL) {
 			printf("%s\n", dir->d_name);
