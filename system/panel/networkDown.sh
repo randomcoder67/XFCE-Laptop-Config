b2b971845293afc33b19dbee75574da8bea7182e
randomcoder67 <minecraft1111311@gmail.com>
2023-09-05 10:25:01 +0100

    ensured width is maintained in panel scripts for network up and down up to 99.9MB/s

--- system/panel/networkDown.sh
+++ system/panel/networkDown.sh
@@ -10,8 +10,14 @@ currentBytes=$(cat /sys/class/net/wlp0s20f3/statistics/rx_bytes)
 previousBytes=$(cat "$filename")
 echo "$currentBytes" > $filename
 
-# Do calculation
-lastTwoSecs=$(echo "$currentBytes $previousBytes" | awk '{ final=($1-$2)/2/1000000 ; printf"<txt>%0.2f MB/s </txt>", final }')
+lastTwoSecs=" "
+
+# Do calculation, ensuring final value only has 3 digits so width is maintained
+if (( $(echo "$currentBytes $previousBytes" | awk '{ final=($1-$2) ; printf"%d", final }') > 19990000 )); then
+	lastTwoSecs=$(echo "$currentBytes $previousBytes" | awk '{ final=($1-$2)/2/1000000 ; printf"<txt>%0.1f MB/s </txt>", final }')
+else
+	lastTwoSecs=$(echo "$currentBytes $previousBytes" | awk '{ final=($1-$2)/2/1000000 ; printf"<txt>%0.2f MB/s </txt>", final }')
+fi
 
 echo "$lastTwoSecs<txtclick>alacritty -e nethogs</txtclick>"
 echo "<tool>NetHogs</tool>"
