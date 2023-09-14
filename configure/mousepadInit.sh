cf588a25f99fb47b76ec7cc15332719c3f1bb00f
randomcoder67 <minecraft1111311@gmail.com>
2023-09-14 23:05:44 +0100

    added Ctrl+Shift+Tab to go to previous tab in Mousepad

--- configure/mousepadInit.sh
+++ configure/mousepadInit.sh
@@ -5,4 +5,5 @@ echo '(gtk_accel_path "<Actions>/win.edit.convert.strip-trailing-spaces" "<Prima
 echo '(gtk_accel_path "<Actions>/win.edit.delete-line" "<Primary>k")' >> ~/.config/Mousepad/accels.scm
 echo '(gtk_accel_path "<Actions>/app.mousepad-plugin-gspell" "<Primary><Shift>k")' >> ~/.config/Mousepad/accels.scm
 echo '(gtk_accel_path "<Actions>/win.document.next-tab" "<Primary>Tab")' >> ~/.config/Mousepad/accels.scm
+echo '(gtk_accel_path "<Actions>/win.document.previous-tab" "<Primary><Shift>Tab")' >> ~/.config/Mousepad/accels.scm
 echo '(gtk_accel_path "<Actions>/win.file.detach-tab" "<Primary><Shift>d")' >> ~/.config/Mousepad/accels.scm
