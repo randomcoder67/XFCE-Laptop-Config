c1b3efe4555b589dceefbd4725542f64f8058542
randomcoder67 <randomcoder67@gmail.com>
2023-11-30 15:22:00 +0000

    fixed days.go program for adding dates past 2069, and updated music.sh to present options when --choice given (mapped to super+shift+l)

--- terminal/alias/music.sh
+++ terminal/alias/music.sh
@@ -16,9 +16,22 @@ fi
 # Check if music is already playing
 ps -ax | grep "/usr/bin/mpv --really-quiet --title=\${metadata/title} - \${metadata/artist} --shuffle --no-resume-playback --loop-playlist $HOME/Music/" | grep -vq "grep" && notify-send "Music already playing" && exit
 
-# If no argument given, shuffle music from current playlist 
+# If no argument given, shuffle music from current playlist
 if [[ "$1" == "" ]]; then
-	/usr/bin/mpv --really-quiet --title='${metadata/title}'\ -\ '${metadata/artist}' --shuffle --no-resume-playback --loop-playlist "$HOME/Music/curPlaylist" --input-ipc-server="$socketName" & disown
+	/usr/bin/mpv --really-quiet --title='${metadata/title}'\ -\ '${metadata/artist}' --shuffle --no-resume-playback --loop-playlist "$HOME/Music/Current Playlist" --input-ipc-server="$socketName" & disown
+# Present choice of playlists
+elif [[ "$1" == "--choice" ]]; then
+	playlists="All Music"$'\n'"$(find $HOME/Music/ -maxdepth 1 -mindepth 1 -type d | sort | sed 's/\([^/]\)\([A-Z]\)/\1 \2/g' | cut -d '/' -f 5)"
+	result=$(echo -e "$playlists" | rofi -dmenu -i -p "Select Music To Play")
+	folder=""
+	if [[ "$result" == "" ]]; then
+		exit
+	elif [[ "$result" == "All Music" ]]; then
+		folder="$HOME/Music"
+	else
+		folder="$HOME/Music/$(echo $result | sed 's/ //g')"
+	fi
+	/usr/bin/mpv --really-quiet --title='${metadata/title}'\ -\ '${metadata/artist}' --shuffle --no-resume-playback --loop-playlist "$folder" --input-ipc-server="$socketName" & disown
 # Shuffle music of a particular artist (or artists with | to delimit)
 elif [[ "$1" == "-a" ]]; then
 	find "$HOME/Music/" -maxdepth 1 -type f | sort | grep -iE ".*-.* $2 .*-.*" | xargs -d '\n' /usr/bin/mpv --really-quiet --title='${metadata/title}'\ -\ '${metadata/artist}' --shuffle --loop-playlist --no-resume-playback --input-ipc-server="$socketName" & disown
