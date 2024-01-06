5df36242774c930babf12218619e38b88d92c0c6
randomcoder67 <minecraft1111311@gmail.com>
2024-01-06 22:25:07 +0000

    youtube len not accepts multiple arguments, added cpu temp for AMD desktop ryzen 5600 commended

--- system/panel/cputemp.sh
+++ system/panel/cputemp.sh
@@ -7,6 +7,7 @@ DEBUG=0
 CPUTEMP=$(sensors | grep -Eo 'Package id 0:.{0,10}' | grep -Eo '[1-9].{0,1}' | sed 's/$/°C/')
 if [[ "$CPUTEMP" == "" ]]; then
 	CPUTEMP=$(sensors | grep CPUTIN | grep -Eo '[1-9].{3}' | head -1 | awk '{ printf"%0.0f", $1}' | sed 's/$/°C/')
+	#CPUTEMP=$(sensors | grep k10temp -A 2 | tail -n 1 | grep -Eo '[1-9].{3}' | awk '{ printf "%0.0f", $1}' | sed 's/$/°C/')
 fi
 
 echo "<txt>$CPUTEMP </txt><txtclick>alacritty -e btop</txtclick>"
