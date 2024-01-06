2b1a272f9bc06aafdb6d4e95b57af60422d56f4c
randomcoder67 <randomcoder67@gmail.com>
2024-01-06 22:25:07 +0000

    youtube len not accepts multiple arguments, added cpu temp for AMD desktop ryzen 5600 commended

--- terminal/webAlternatives/youtubeLength.sh
+++ terminal/webAlternatives/youtubeLength.sh
@@ -2,18 +2,21 @@
 
 # Script to find the total length of a YouTube playlist
 
-# Get playlist data into an array (yt-dlp -j gives each video as a seperate JSON block)
-oldIFS="$IFS"
-IFS=$'\n'
-playlistJSON=( $(yt-dlp --cookies-from-browser firefox -q -j --flat-playlist "$1" 2>/dev/null) )
-IFS="$oldIFS"
-
-# Add up duration
 totalDuration=0
-for entry in "${playlistJSON[@]}"; do
-	videoDuration=$(echo $entry | jq .duration)
-	totalDurationA=$((totalDuration+videoDuration))
-	totalDuration=$totalDurationA
+
+for arg; do
+	# Get playlist data into an array (yt-dlp -j gives each video as a seperate JSON block)
+	oldIFS="$IFS"
+	IFS=$'\n'
+	playlistJSON=( $(yt-dlp --cookies-from-browser firefox -q -j --flat-playlist -- "$arg" 2>/dev/null) )
+	IFS="$oldIFS"
+
+	# Add up duration
+	for entry in "${playlistJSON[@]}"; do
+		videoDuration=$(echo $entry | jq .duration)
+		totalDurationA=$((totalDuration+videoDuration))
+		totalDuration=$totalDurationA
+	done
 done
 
 # Calculate hours, minutes and seconds
