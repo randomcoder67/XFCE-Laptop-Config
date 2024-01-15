0208a5209bdd3dd0cd95c1b5346294a6a7970344
randomcoder67 <randomcoder67@gmail.com>
2024-01-15 14:31:06 +0000

    added background music option, moved choice to super+o, fixed bug in log.go

--- terminal/alias/music.sh
+++ terminal/alias/music.sh
@@ -15,10 +15,27 @@ elif [[ "$1" == "--prev" ]]; then
 elif [[ "$1" == "--favourite" ]]; then
 	currentlyPlaying=$(echo '{ "command": ["get_property", "path"] }' | socat - "$socketName" | jq .data -r)
 	cp "$currentlyPlaying" "$favouritesDir"
+	notify-send "Added song to favourites"
+	exit
+elif [[ "$1" == "--toggle-pause" ]]; then
+	echo "cycle pause" | socat - "$socketName"
 	exit
 elif [[ "$1" == "--quit" ]]; then
 	echo "quit" | socat - "$socketName"
 	exit
+fi
+
+backgroundArg1=""
+backgroundArg2=""
+doArg="$1"
+
+if [[ "$1" == "--background" ]]; then
+	backgroundArg1="--no-audio-display"
+	backgroundArg2="--force-window=no"
+	doArg=""
+elif [[ "$2" == "--background" ]]; then
+	backgroundArg1="--no-audio-display"
+	backgroundArg2="--force-window=no"
 fi
 
 # Check if music is already playing
@@ -31,10 +48,10 @@ ps -ax | grep "/usr/bin/mpv --really-quiet --title=\${metadata/title} - \${metad
 #fi
 
 # If no argument given, shuffle music from current playlist
-if [[ "$1" == "" ]]; then
-	/usr/bin/mpv --really-quiet --title='${metadata/title}'\ -\ '${metadata/artist}' --shuffle --no-resume-playback --loop-playlist "$HOME/Music/CurrentPlaylist" --input-ipc-server="$socketName" & disown
+if [[ "$doArg" == "" ]]; then
+	/usr/bin/mpv --really-quiet --title='${metadata/title}'\ -\ '${metadata/artist}' --shuffle --no-resume-playback --loop-playlist "$HOME/Music/CurrentPlaylist" "$backgroundArg1" "$backgroundArg2" --input-ipc-server="$socketName" & disown
 # Present choice of playlists
-elif [[ "$1" == "--choice" ]]; then
+elif [[ "$doArg" == "--choice" ]]; then
 	playlists="All Music"$'\n'"$(find $HOME/Music/ -maxdepth 1 -mindepth 1 -type d | sort | sed 's/\([^/]\)\([A-Z][a-z]\)/\1 \2/g' | sed 's/\([a-z]\)\([0-9]\)/\1 \2/g' | cut -d '/' -f 5)"
 	result=$(echo -e "$playlists" | rofi -dmenu -i -p "Select Music To Play")
 	folder=""
@@ -45,10 +62,10 @@ elif [[ "$1" == "--choice" ]]; then
 	else
 		folder="$HOME/Music/$(echo $result | sed 's/ //g')"
 	fi
-	/usr/bin/mpv --really-quiet --title='${metadata/title}'\ -\ '${metadata/artist}' --shuffle --no-resume-playback --loop-playlist "$folder" --input-ipc-server="$socketName" & disown
+	/usr/bin/mpv --really-quiet --title='${metadata/title}'\ -\ '${metadata/artist}' --shuffle --no-resume-playback --loop-playlist "$folder" "$backgroundArg1" "$backgroundArg2" --input-ipc-server="$socketName" & disown
 # Shuffle music of a particular artist (or artists with | to delimit)
-elif [[ "$1" == "-a" ]]; then
-	find "$HOME/Music/" -maxdepth 1 -type f | sort | grep -iE ".*-.* $2 .*-.*" | xargs -d '\n' /usr/bin/mpv --really-quiet --title='${metadata/title}'\ -\ '${metadata/artist}' --shuffle --loop-playlist --no-resume-playback --input-ipc-server="$socketName" & disown
-elif [[ "$1" == "-al" ]]; then
-	/usr/bin/mpv --really-quiet --title='${metadata/title}'\ -\ '${metadata/artist}' --shuffle --no-resume-playback --loop-playlist "$HOME/Music" --input-ipc-server="$socketName" & disown
+elif [[ "$doArg" == "-a" ]]; then
+	find "$HOME/Music/" -maxdepth 1 -type f | sort | grep -iE ".*-.* $2 .*-.*" | xargs -d '\n' /usr/bin/mpv --really-quiet --title='${metadata/title}'\ -\ '${metadata/artist}' --shuffle --loop-playlist --no-resume-playback "$backgroundArg1" "$backgroundArg2" --input-ipc-server="$socketName" & disown
+elif [[ "$doArg" == "-al" ]]; then
+	/usr/bin/mpv --really-quiet --title='${metadata/title}'\ -\ '${metadata/artist}' --shuffle --no-resume-playback --loop-playlist "$HOME/Music" "$backgroundArg1" "$backgroundArg2" --input-ipc-server="$socketName" & disown
 fi
