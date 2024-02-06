d846d20f206224cdf88a7e0e6df2a43fcc9303c9
randomcoder67 <randomcoder67@gmail.com>
2024-02-06 15:20:35 +0000

    big changes to panel stream monitors. Archived all the old ones, made a new generic one and updated panel config accordingly

--- system/panel/streamLauncher.sh
+++ system/panel/streamLauncher.sh
@@ -2,32 +2,14 @@
 
 # Program to open livestreams in mpv and give appropriate error messages if they fail to open
 
-if [[ "$1" == "matnYouTube" ]]; then
-	notify-send "Opening Many A True Nerd YouTube Stream"
-	mpv --ytdl-format=best --title='Many A True Nerd - YouTube' https://www.youtube.com/@ManyATrueNerd/live || notify-send "Error, stream failed to open"
-elif [[ "$1" == "nmixxYouTube" ]]; then
-	notify-send "Opening NMIXX YouTube Stream"
-	mpv --ytdl-format=best --title='NMIXX - YouTube' https://www.youtube.com/@NMIXXOfficial/live || notify-send "Error, stream failed to open"
-elif [[ "$1" == "chudlogicYouTube" ]]; then
-	notify-send "Opening Chud Logic YouTube Stream"
-	mpv --ytdl-format=best --title='Chud Logic - YouTube' https://www.youtube.com/@ChudLogic/live || notify-send "Error, stream failed to open"
-elif [[ "$1" == "destinyYouTube" ]]; then
-	notify-send "Opening Destiny YouTube Stream"
-	mpv --ytdl-format=best --title='Destiny - YouTube' https://www.youtube.com/@Destiny/live || notify-send "Error, stream failed to open"
-elif [[ "$1" == "chudlogicTwitch" ]]; then
-	notify-send "Opening Chud Logic Twitch Stream"
-	mpv --ytdl-format=best --title='Chud Logic - Twitch' https://www.twitch.tv/chudlogic || notify-send "Error, stream failed to open"
-elif [[ "$1" == "nerdcubedTwitch" ]]; then
-	notify-send "Opening NerdCubed Twitch Stream"
-	mpv --ytdl-format=best --title='NerdCubed - Twitch' https://www.twitch.tv/nerdcubed || notify-send "Error, stream failed to open"
-elif [[ "$1" == "dustinedenTwitch" ]]; then
-	notify-send "Opening Dustin Eden Twitch Stream"
-	mpv --ytdl-format=best --title='Dustin Eden - Twitch' https://www.twitch.tv/dustineden || notify-send "Error, stream failed to open"
-elif [[ "$1" == "destinyRumble" ]]; then
-	notify-send "Opening Destiny Rumble Stream"
-	"$HOME/Programs/system/panel/destinyRumble.sh" || notify-send "Error, stream failed to open"
-elif [[ "$1" == "noahsundayYouTube" ]]; then
-	notify-send "Opening Noah Sunday YouTube Stream"
-	mpv --ytdl-format=best --title='Noah Sunday - YouTube' https://www.youtube.com/@NoahSundayCompletionist/live || notify-send "Error, stream failed to open"
-fi
+platform="$1"
+channelName="$2"
+channelAt="$3"
 
+if [[ "$platform" == "YouTube" ]]; then
+	notify-send "Opening ${channelName} YouTube Stream"
+	mpv --ytdl-format=best --title="${channelName} - YouTube" "https://www.youtube.com/@${channelAt}/live" || notify-send "Error, stream failed to open"
+elif [[ "$platform" == "Twitch" ]]; then
+	mpv --ytdl-format=best --title="${channelName} - Twitch" "https://www.twitch.tv/${channelAt}" || notify-send "Error, stream failed to open"
+	notify-send "Opening ${channelName} Twitch Stream"
+fi
