f67cfe99db6c6c2828d555c76f9a5168c4efce05
randomcoder67 <randomcoder67@gmail.com>
2024-02-07 12:00:10 +0000

    minor fixes

--- system/panel/uptime.sh
+++ system/panel/uptime.sh
@@ -6,6 +6,10 @@ uptimeFormatted=$(uptime | tr -s " " | cut -d " " -f 4 | grep -o [0-9:]*)
 
 if [[ "${#uptimeFormatted}" == "4" ]]; then
 	uptimeFormatted="0${uptimeFormatted}"
+elif [[ "${#uptimeFormatted}" == "2" ]]; then
+	uptimeFormatted="00:${uptimeFormatted}"
+elif [[ "${#uptimeFormatted}" == "1" ]]; then
+	uptimeFormatted="00:0${uptimeFormatted}"
 fi
 
 echo "<txt> - ${uptimeFormatted}</txt>"
