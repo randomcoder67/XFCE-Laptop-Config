8bb5ae9308094faceff7c93d22c150997e70253a
randomcoder67 <randomcoder67@gmail.com>
2024-02-14 18:35:59 +0000

    added details to tooltip of uptime panel script

--- system/panel/uptime.sh
+++ system/panel/uptime.sh
@@ -2,7 +2,13 @@
 
 # Script to display uptime
 
-uptimeFormatted=$(uptime | tr -s " " | cut -d " " -f 4 | grep -o [0-9:]*)
+uptimeNormal=$(uptime | tr -s " ")
+uptimeFormatted=$(echo "$uptimeNormal" | cut -d " " -f 4 | grep -o [0-9:]*)
+uptimeSince="$(uptime -s)"
+
+loadOneMinute=$(echo "$uptimeNormal" | cut -d " " -f 9 | grep -o [0-9.]*)
+loadFiveMinutes=$(echo "$uptimeNormal" | cut -d " " -f 10 | grep -o [0-9.]*)
+loadFifteenMinutes=$(echo "$uptimeNormal" | cut -d " " -f 11 | grep -o [0-9.]*)
 
 if [[ "${#uptimeFormatted}" == "4" ]]; then
 	uptimeFormatted="0${uptimeFormatted}"
@@ -13,4 +19,8 @@ elif [[ "${#uptimeFormatted}" == "1" ]]; then
 fi
 
 echo "<txt> - ${uptimeFormatted}</txt>"
-echo "<tool>Uptime</tool>"
+echo "<tool>Uptime Details
+Up Since: ${uptimeSince}
+Load Average (1m): ${loadOneMinute}
+Load Average (5m): ${loadFiveMinutes}
+Load Average (15m): ${loadFifteenMinutes}</tool>"
