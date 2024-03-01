a9a1ab1cac5addc6b4e68f34c396b535befccc0d
randomcoder67 <minecraft1111311@gmail.com>
2024-03-01 11:43:33 +0000

    added pyweb alias for easy website testing

--- system/keyboardOther/altGrToSuper.sh
+++ system/keyboardOther/altGrToSuper.sh
@@ -5,5 +5,6 @@ xmodmap -e 'remove mod5 = ISO_Level3_Shift'
 # Map it to Super_L
 xmodmap -e 'keycode 108 = Super_L'
 
-# ksuperkey has to be launched after doing this, otherwise it will not recognise AltGr as Super
+# ksuperkey has to be launched/restarted after doing this, otherwise it will not recognise AltGr as Super
+pkill ksuperkey
 ksuperkey
