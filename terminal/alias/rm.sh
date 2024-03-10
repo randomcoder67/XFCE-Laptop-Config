d7b36e786cb1d55d0e1569e0b71c4dd707f5df36
randomcoder67 <randomcoder67@gmail.com>
2024-03-10 10:56:24 +0000

    mv.sh and rm.sh default to normal commands (with warning) when trash-cli not installed

--- terminal/alias/rm.sh
+++ terminal/alias/rm.sh
@@ -2,6 +2,13 @@
 
 # rm script, allows deleting directories or files with same command, and changes rm to use trash-put. Also disallows rm ~ and rm /
 
+if ! command -v trash-put &> /dev/null; then
+    echo "Error: trash-cli not found. Please install it."
+    echo "WARNING: trash-cli not installed, using rm as normal"
+    rm "$@"
+    exit
+fi
+
 for arg; do
 	# Checks if you are trying to delete the home or root directory, if so, refuses
 	if [ "$arg" == "$HOME" ] || [ "$arg" == "/" ]; then
