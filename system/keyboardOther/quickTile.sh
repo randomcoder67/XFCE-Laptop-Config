a70c81e988468477e040889c84c78031d7a09953
randomcoder67 <minecraft1111311@gmail.com>
2024-03-17 21:47:06 +0000

    added quickTile.sh

---
+++ system/keyboardOther/quickTile.sh
@@ -0,0 +1,49 @@
+#!/usr/bin/env bash
+
+# Script to open Firefox and two terminals in predefined format
+
+# Open windows
+firefox & disown
+alacritty & disown
+alacritty & disown
+
+sleep 1
+
+currentWorkspace=$(wmctrl -d | grep \* | tr -s " " | cut -d' ' -f10)
+workspaceNum=" 0"
+if [[ "$currentWorkspace" == "Sec" ]]; then
+	workspaceNum=" 1"
+fi
+
+windows=$(wmctrl -lG | grep -P "^.{11}$workspaceNum" | tr -s ' ')
+
+# Get the stack of windows
+stack=( $(xprop -root | grep "STACKING(WINDOW)" | cut -d "#" -f 2 | tac -s ' ' | sed 's/,//g' | sed 's/0x/0x0/g') )
+
+# Filter the stack array to only windows on the current desktop
+stackFiltered=()
+for x in "${stack[@]}"; do
+	if [[ "$windows" = *"$x"* ]]; then
+		stackFiltered+=("$x")
+	fi
+done
+
+doneA=0
+
+for windowID in "${stackFiltered[@]}"; do
+	wmctrl -r "$windowID" -i -b add,maxmized_vert
+	if echo "$windows" | grep "$windowID" | grep -i "firefox"; then
+		xdotool windowsize "$windowID" 1476 1330
+		xdotool windowmove "$windowID" 0 36
+	elif [[ "$doneA" == 0 ]]; then
+		xdotool windowsize "$windowID" 756 761
+		xdotool windowmove "$windowID" 1480 36
+		doneA=1
+	else
+		xdotool windowsize "$windowID" 756 538
+		xdotool windowmove "$windowID" 1480 828
+		break
+	fi 
+done
+
+notify-send "Windows Arranged"
