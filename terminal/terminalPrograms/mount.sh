52cfac78c93cc2972154a42666184bb829a082bd
randomcoder67 <minecraft1111311@gmail.com>
2024-05-23 18:00:23 +0100

    added more options to mount.sh

--- terminal/terminalPrograms/mount.sh
+++ terminal/terminalPrograms/mount.sh
@@ -8,8 +8,10 @@ lsblk -o NAME,MAJ:MIN,RM,SIZE,RO,TYPE,FSTYPE,MOUNTPOINTS
 echo "Select Option to Mount:"
 echo "1. /dev/sda1 -> ~/Downloads/BackupMount"
 echo "2. /dev/sda1 -> ~/Downloads/USBDrive"
-echo "3. /dev/sda -> ~/Downloads/USBDrive"
-echo "4. Unmount"
+echo "3. /dev/sda1 -> ~/Downloads/USBDrive (FAT)"
+echo "4. /dev/sda -> ~/Downloads/USBDrive"
+echo "5. /dev/sda -> ~/Downloads/USBDrive (FAT)"
+echo "6. Unmount"
 read -p "Enter option (q to quit): " inputVar
 # Check for q or blank and exit
 [ "$inputVar" == "" ] || [ "$inputVar" == "q" ] && exit
@@ -17,8 +19,10 @@ read -p "Enter option (q to quit): " inputVar
 # Handle given option
 [ "$inputVar" == "1" ] && sudo mount /dev/sda1 ~/Downloads/BackupMount
 [ "$inputVar" == "2" ] && sudo mount /dev/sda1 ~/Downloads/USBDrive
-[ "$inputVar" == "3" ] && sudo mount /dev/sda ~/Downloads/USBDrive
-if [ "$inputVar" == "4" ]; then
+[ "$inputVar" == "3" ] && sudo mount -o rw,users,umask=000 /dev/sda1 ~/Downloads/USBDrive
+[ "$inputVar" == "4" ] && sudo mount /dev/sda ~/Downloads/USBDrive
+[ "$inputVar" == "5" ] && sudo mount -o rw,users,umask=000 /dev/sda ~/Downloads/USBDrive
+if [ "$inputVar" == "6" ]; then
 	mounted=$(lsblk | grep sda | grep -E "part|disk /" | cut -d " " -f 1 | tr -d "└─")
 	[[ "$mounted" == "" ]] && exit
 	sudo umount "/dev/$mounted"
