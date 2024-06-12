35a63f908aa7b355beae19a27005d34df11aa92b
randomcoder67 <minecraft1111311@gmail.com>
2024-06-12 21:13:30 +0100

    modified mpv scripts, oscPeek.lua now has osc on by default, except for audio files, and osc_tethys now has a resolution display in the bottom bar

--- configure/home/.config/mpv/scripts/oscPeek.lua
+++ configure/home/.config/mpv/scripts/oscPeek.lua
@@ -3,10 +3,16 @@ local CLEAR_OSD_TIMEOUT = .01
 
 local osc_always_on = false
 
+local options = {
+	oscactive = true
+}
+
+require "mp.options".read_options(options, "oscpeek")
+
 local function toggle_osc_auto_always()
 	osc_always_on = not osc_always_on
 	
-	mp.commandv('script-message', 'osc-visibility', osc_always_on and 'always' or 'auto')
+	mp.commandv('script-message', 'osc-visibility', osc_always_on and 'always' or 'auto', "")
 	mp.add_timeout(CLEAR_OSD_TIMEOUT, function () mp.osd_message('') end)
 end
 
@@ -17,5 +23,17 @@ local function peek_osc()
 	end
 end
 
+local function initialise()
+	filename = mp.get_property_native("filename")
+	if filename:match(".m4a$") or filename:match(".mp3$") then
+		osc_always_on = true
+	else
+		osc_always_on = not options.oscactive
+	end
+	toggle_osc_auto_always()
+end
+
 mp.add_key_binding(nil, 'toggle-osc-auto-always', toggle_osc_auto_always)
 mp.add_key_binding(nil, 'peek-osc', peek_osc)
+
+mp.register_event("file-loaded", initialise)
